e986ffc1542da5d74dbf2fbc83b4d2e1f3bfb0ee
Nikita Hovratov <nikita.h@live.de>
2021-01-23 13:55:50 +0100

    Remove unneeded json conversion

--- Resources/Public/JavaScript/Mask.js
+++ Resources/Public/JavaScript/Mask.js
@@ -568,14 +568,14 @@ define([
         return true;
       },
       getNewElement: function () {
-        return JSON.parse(JSON.stringify({
+        return {
           key: '',
           label: '',
           shortLabel: '',
           description: '',
           icon: '',
           color: '#000000'
-        }));
+        };
       },
       isEmptyObject: function (obj) {
         return Object.keys(obj).length === 0 && obj.constructor === Object;
